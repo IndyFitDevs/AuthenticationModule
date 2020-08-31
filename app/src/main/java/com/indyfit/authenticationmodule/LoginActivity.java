3569a7fa4c7098d5ebe1b083bbc4949ab902743f
Parth Sharma <parthsharma151@gmail.com>
2020-08-31 12:24:31 +0530

    added button functionality, login using entered username and password, new intent

--- app/src/main/java/com/indyfit/authenticationmodule/LoginActivity.java
+++ app/src/main/java/com/indyfit/authenticationmodule/LoginActivity.java
@@ -2,7 +2,11 @@ package com.indyfit.authenticationmodule;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -25,18 +29,42 @@ import okhttp3.Response;
 
 public class LoginActivity extends AppCompatActivity {
 
+    // UI elements initialization
+    EditText email_username;
+    EditText user_password;
+    Button login;
+
+    // Variable initialization
     String api_key = "";
+    String email_username_string = "";
+    String user_password_string = "";
+
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
         AndroidNetworking.initialize(getApplicationContext());
 
+        email_username = (EditText) findViewById(R.id.editText_email);
+        user_password = (EditText) findViewById(R.id.editText_password);
+        login = (Button) findViewById(R.id.button_login);
+
+
+    }
+
+    public void attempt_login(View view){
+
+        //TODO: exception handling in username
+
+        email_username_string = email_username.getText().toString();
+        user_password_string = user_password.getText().toString();
+
         // code to POST data to API
         JSONObject jsonObject = new JSONObject();
         try {
-            jsonObject.put("username", "indyfit.tester@yopmail.com");
-            jsonObject.put("password", "testtest1234!");
+            jsonObject.put("username", email_username_string);
+            jsonObject.put("password", user_password_string);
         } catch (JSONException e) {
             e.printStackTrace();
         }
@@ -51,8 +79,10 @@ public class LoginActivity extends AppCompatActivity {
                         Toast toast = Toast. makeText(getApplicationContext(), "Successfully " +
                                         "logged in.",
                                 Toast. LENGTH_SHORT);
-
                         toast.show();
+
+                        Intent intent = new Intent(LoginActivity.this, SuccessActivity.class);
+                        startActivity(intent);
                     }
 
                     @Override
@@ -62,6 +92,7 @@ public class LoginActivity extends AppCompatActivity {
                         toast.show();
                     }
                 });
+
 
 
     }
